9d397340303edbe5ebc81efb73fba0172c16cf5c
Sunnie Lin <sunnie1239@gmail.com>
2024-02-01 23:06:02 -0800

    update function

--- leap.py
+++ leap.py
@@ -1,12 +1,14 @@
 def is_leap(year):
 	if year % 4 != 0:
 		return False
-	elif year % 4 == 0 and year % 100 != 0:
+	elif year % 100 != 0:
 		return True
-	elif year % 100 == 0 and year % 400 != 0:
+	elif year % 400 != 0:
 		return False
-	elif year % 100 == 0 and year % 3200 != 0:
+	elif year % 3200 != 0:
 		return True
+	else:
+		return False	
 
 year = int(input('Please enter a year: '))
 print(is_leap(year))
